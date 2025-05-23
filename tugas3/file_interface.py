25fc653daf6a4d41276675aef4e8f0a89bf7682e
vinsa61 <145869213+vinsa61@users.noreply.github.com>
2025-05-23 18:16:52 +0700

    Add files via upload

---
+++ tugas3/file_interface.py
@@ -0,0 +1,59 @@
+import os
+import json
+import base64
+from glob import glob
+
+
+class FileInterface:
+    def __init__(self):
+        os.chdir('files/')
+
+    def list(self,params=[]):
+        try:
+            filelist = glob('*.*')
+            return dict(status='OK',data=filelist)
+        except Exception as e:
+            return dict(status='ERROR',data=str(e))
+
+    def get(self,params=[]):
+        try:
+            filename = params[0]
+            if (filename == ''):
+                return None
+            fp = open(f"{filename}",'rb')
+            isifile = base64.b64encode(fp.read()).decode()
+            return dict(status='OK',data_namafile=filename,data_file=isifile)
+        except Exception as e:
+            return dict(status='ERROR',data=str(e))
+
+    def upload(self, params=[]):
+        try:
+            filename = params[0]
+            filedata = params[1]
+            if filename == '' or filedata == '':
+                return dict(status='ERROR', data='filename or filedata missing')
+
+            with open(filename, 'wb') as f:
+                f.write(base64.b64decode(filedata))
+
+            return dict(status='OK', data=f'{filename} uploaded')
+        except Exception as e:
+            return dict(status='ERROR', data=str(e))
+
+    def delete(self, params=[]):
+        try:
+            filename = params[0]
+            if os.path.exists(filename):
+                os.remove(filename)
+                return dict(status='OK', data=f'{filename} deleted')
+            else:
+                return dict(status='ERROR', data='file not found')
+        except Exception as e:
+            return dict(status='ERROR', data=str(e))
+
+
+
+if __name__=='__main__':
+    f = FileInterface()
+    print(f.list())
+    print(f.get(['pokijan.jpg']))
---
+++ tugas3/file_server.py
@@ -0,0 +1,69 @@
+from socket import *
+import socket
+import threading
+import logging
+import time
+import sys
+
+
+from file_protocol import  FileProtocol
+fp = FileProtocol()
+
+
+class ProcessTheClient(threading.Thread):
+    def __init__(self, connection, address):
+        self.connection = connection
+        self.address = address
+        threading.Thread.__init__(self)
+
+    def run(self):
+        buffer = ""
+        delimiter = "\r\n\r\n"
+        
+        while True:
+            data = self.connection.recv(209715200)
+            if not data:
+                break 
+            
+            buffer += data.decode()
+            
+            if delimiter in buffer:
+                break
+
+        if buffer:
+            d = buffer.rstrip(delimiter)
+            hasil = fp.proses_string(d)
+            hasil = hasil + delimiter
+            self.connection.sendall(hasil.encode())
+        
+        self.connection.close()
+
+
+class Server(threading.Thread):
+    def __init__(self,ipaddress='0.0.0.0',port=8889):
+        self.ipinfo=(ipaddress,port)
+        self.the_clients = []
+        self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        threading.Thread.__init__(self)
+
+    def run(self):
+        logging.warning(f"server berjalan di ip address {self.ipinfo}")
+        self.my_socket.bind(self.ipinfo)
+        self.my_socket.listen(1)
+        while True:
+            self.connection, self.client_address = self.my_socket.accept()
+            logging.warning(f"connection from {self.client_address}")
+
+            clt = ProcessTheClient(self.connection, self.client_address)
+            clt.start()
+            self.the_clients.append(clt)
+
+
+def main():
+    svr = Server(ipaddress='0.0.0.0',port=6666)
+    svr.start()
+
+
+if __name__ == "__main__":
+    main()
